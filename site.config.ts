63441b461380507721a466c67b8bc3823068fb2a
THU-Pieris <yogabear123456@gmail.com>
2022-05-31 16:55:20 +0800

    test

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '949c588e312042d0b20872f0234695ca',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Pieris Blog',
+  domain: 'notion.pieris05.com',
+  author: 'Pieris',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
